6a054099d670dc9ce8c6a4eb4a5b26edf7c9f326
Aethus Northcott <aethus@brocktonpoint.com>
2017-08-18 16:43:40 -0700

    Add swiftlint

--- Sources/CawToken.swift
+++ Sources/CawToken.swift
@@ -9,10 +9,10 @@ struct CawToken {
     }
 
     init (length: Int) {
-        let max = UInt32(CawToken.characters.count)
+        let max = UInt32 (CawToken.characters.count)
         let results: [String] = (0 ..< length).map { _ in
-            let position = Int(arc4random_uniform(max))
-            let character = CawToken.characters.index(CawToken.characters.startIndex, offsetBy: position)
+            let position = Int (arc4random_uniform(max))
+            let character = CawToken.characters.index (CawToken.characters.startIndex, offsetBy: position)
             return String (CawToken.characters[character])
         }
 
