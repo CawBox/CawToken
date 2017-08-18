1d4161f02a95f922b403642f14ab92cde0d51a3b
Aethus Northcott <aethus@brocktonpoint.com>
2017-08-18 16:46:38 -0700

    Expose items as public

--- Sources/CawToken.swift
+++ Sources/CawToken.swift
@@ -1,14 +1,14 @@
 import Foundation
 
-struct CawToken {
+public struct CawToken {
     fileprivate static let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    let value: String
+    public let value: String
 
-    static func create (_ length: Int) -> String {
+    public static func create (_ length: Int) -> String {
         return CawToken (length: length).value
     }
 
-    init (length: Int) {
+    public init (length: Int) {
         let max = UInt32 (CawToken.characters.count)
         let results: [String] = (0 ..< length).map { _ in
             let position = Int (arc4random_uniform(max))
