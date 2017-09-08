a9c8eec401ade7f0b57d482c3d4b71a682701623
Aethus Northcott <aethus@brocktonpoint.com>
2017-09-07 19:42:49 -0700

    Run swiftformat

--- Sources/CawToken.swift
+++ Sources/CawToken.swift
@@ -4,18 +4,18 @@ public struct CawToken {
     fileprivate static let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
     public let value: String
 
-    public static func create (_ length: Int) -> String {
-        return CawToken (length: length).value
+    public static func create(_ length: Int) -> String {
+        return CawToken(length: length).value
     }
 
-    public init (length: Int) {
-        let max = UInt32 (CawToken.characters.count)
+    public init(length: Int) {
+        let max = UInt32(CawToken.characters.count)
         let results: [String] = (0 ..< length).map { _ in
-            let position = Int (arc4random_uniform(max))
-            let character = CawToken.characters.index (CawToken.characters.startIndex, offsetBy: position)
-            return String (CawToken.characters[character])
+            let position = Int(arc4random_uniform(max))
+            let character = CawToken.characters.index(CawToken.characters.startIndex, offsetBy: position)
+            return String(CawToken.characters[character])
         }
 
-        self.value = results.joined()
+        value = results.joined()
     }
 }
